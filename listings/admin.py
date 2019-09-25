8486001b6129e8273003d12b4bf73340b1b9f87d
wcolts2000 <wcolts2000@gmail.com>
2019-09-25 11:21:03 -0700

    added filter and search fields and links to listings admin

--- listings/admin.py
+++ listings/admin.py
@@ -6,6 +6,12 @@ from .models import Listing
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published',
                     'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state')
+    list_per_page = 25
 
 
 admin.site.register(Listing, ListingAdmin)
