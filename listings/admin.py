36cabf8ba352d2a4ef8569190a2e6468bfa32fa1
wcolts2000 <wcolts2000@gmail.com>
2019-09-25 10:25:13 -0700

    added missing second param to admin.site.register method to get the list displaying correctly in the admin panel

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+
+
+admin.site.register(Listing, ListingAdmin)
