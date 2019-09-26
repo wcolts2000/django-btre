233029eb0e665275f97a2d008b18455bdd5e7eb8
wcolts2000 <wcolts2000@gmail.com>
2019-09-26 13:29:23 -0700

    made about us page dynamic

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -15,4 +16,16 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+      'realtors': realtors,
+      'mvp_realtors': mvp_realtors
+    }
+
+
+    return render(request, 'pages/about.html', context)
