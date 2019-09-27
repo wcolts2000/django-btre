0c050e275f66a8e5ca211b39f9b6e5a3d2e50254
wcolts2000 <wcolts2000@gmail.com>
2019-09-27 13:35:23 -0700

    added register functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,51 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check that passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "That email is being used already")
+                    return redirect('register')
+                else: 
+                    # Looks good
+                    user = User.objects.create_user(username=username, password=password, email=email, last_name=last_name, first_name=first_name)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, "You are now logged in")
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, "You are now registered and can log in")
+                    return redirect('login')
+
+            return 
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Login User
+        return
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
