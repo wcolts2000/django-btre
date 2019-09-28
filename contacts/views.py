4005293b58947e507e04731783f8149d99bd9ead
wcolts2000 <wcolts2000@gmail.com>
2019-09-27 18:59:08 -0700

    conpleted hooking up inquiry form and setting email sending capanbility on inquiries, left fields empty as to not push passwords to github

--- contacts/views.py
+++ contacts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
     if request.method == "POST":
         listing_id = request.POST['listing_id']
-        listing = request.POST['listing_id']
+        listing = request.POST['listing']
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
@@ -13,9 +14,26 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Check if user had made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        # Send email
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry on ' + listing + '. Sign into the admin panel for more info',
+            'wcolts2000@gmail.com',
+            [realtor_email, 'wcolts2000@gmail.com'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
